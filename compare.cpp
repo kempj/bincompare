378becba0d0d02d3a2b8e71a6d2b231fad695097
Jeremy Kemp <jakemp@uh.edu>
2014-12-03 14:30:26 -0600

    reformatted the output for a range of tolerances

--- compare.cpp
+++ compare.cpp
@@ -4,9 +4,11 @@
 #include <vector>
 #include <iterator>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
+int line_len = 100;
 vector<float> get_data(char *filename)
 {
     ifstream file( filename, ios::binary );
@@ -27,36 +29,55 @@ vector<float> get_data(char *filename)
 
     return buffer;
 }
+void print_line(int N) {
+    for(int i = 0; i < N; i++) 
+        cout << "-";
+    cout << endl;
+}
 
+void print_header(){
+    print_line(line_len);
+    cout << "| " << left << setw(10) << "tolerance";
+    cout << "| " << setw(20 + 3) << "# non-equal elements" ;
+    cout << "| " << setw(13) << "sum of diffs";
+    cout << "| " << setw(12) << "avg diff";
+    cout << "| " << setw(12) << "max diff" ;
+    cout << endl;
+    print_line(line_len);
+}
 void analyze(vector<float> buff1, vector<float> buff2, float tolerance) 
 {
-    vector<float> buff3(buff1.size());
+    //vector<float> buff3(buff1.size());
     int num_different = 0;
     float sum_difference = 0;
     float biggest_percent = 0;
     int biggest_diff_idx = -1;
 
     for(int i = 0; i < buff1.size(); i++) {
-        buff3[i] = buff2[i] - buff1[i];
-        sum_difference += buff3[i];
-        if(buff3[i] > tolerance) {
+        float val = buff2[i] - buff1[i];
+        sum_difference += val;
+        if(val > tolerance) {
             num_different++;
         }
-        if(buff3[i] > tolerance && buff1[i] > tolerance) {
-            float percent_diff = 100.0 *(buff3[i] / buff1[i]);
+        if(val > tolerance && buff1[i] > tolerance) {
+            float percent_diff = 100.0 *(val / buff1[i]);
             if(percent_diff > biggest_percent) {
                 biggest_percent = percent_diff;
                 biggest_diff_idx = i;
             }
         }
     }
-    cout << endl << "tolerance = " << tolerance << endl;
-    cout << "number of non-equal elements: " << num_different
-         << " / " << buff3.size() << endl;
-    cout << "total sum of difference: " << sum_difference << endl;
-    cout << "average over all elements: " << (sum_difference / (float)buff3.size()) << endl;
-    cout << "average difference for non-equal elements: " << (sum_difference / (float)num_different) << endl;
-    cout << "biggest percent difference: " << biggest_percent << "%";
+    float avg_diff;
+    if(num_different == 0) {
+        avg_diff = 0;
+    } else {
+        avg_diff = (sum_difference / (float)num_different);
+    }
+    cout << "| " << setw(10) << tolerance 
+         << "| " << setw(10) << right << num_different << " / " << left << setw(10) << buff1.size() 
+         << "| " << setw(13) << sum_difference 
+         << "| " << setw(12) << avg_diff
+         << "| " << biggest_percent << "%";
     if(biggest_diff_idx > 0) {
         cout << " ( " << buff1[biggest_diff_idx] << " vs " << buff2[biggest_diff_idx] << " )";
     }
@@ -65,13 +86,13 @@ void analyze(vector<float> buff1, vector<float> buff2, float tolerance)
 
 int main( int argc, char **argv )
 {
-    float tolerance = .000001;
+    float tolerance = -6;
     if( argc < 3 ) {
         cout << "requires 2 files names" << endl;
         return 0;
     }
     if( argc > 3) {
-        tolerance = pow(10, atoi(argv[3]));
+        tolerance = atoi(argv[3]);
     }
 
     vector<float> buff1 = get_data(argv[1]);
@@ -84,7 +105,10 @@ int main( int argc, char **argv )
         return 0;
     }
 
-    analyze(buff1, buff2, tolerance);
-
+    print_header();
+    for(int tol = tolerance-3; tol < tolerance+4; tol++) {
+        analyze(buff1, buff2, pow(10.0, tol));
+    }
+    print_line(line_len);
     return 0;
 }
