2e6607e82887203591b1c87d782e97e06da1addd
Jeremy Kemp <jakemp@uh.edu>
2014-12-03 18:28:33 -0600

    removed print functions from compare

--- compare.cpp
+++ compare.cpp
@@ -85,79 +85,6 @@ void analyze(vector<float> buff1, vector<float> buff2, float tolerance)
     cout << endl;
 }
 
-void print_diff(vector<float> buff1, vector<float> buff2, float tolerance){
-    int count = 0;
-    for(int i = 0; i < buff1.size(); i++) {
-        float diff = buff1[i] = buff2[i];
-        if(abs(diff) > tolerance) {
-            cout << left << "(i=" << i << ": " << setw(12) << diff << " - " << setw(12) << buff1[i] <<  ") ";
-            count++;
-            if(count % 3 == 0) {
-                cout << endl;
-                if(count == 120) {
-                    count = 0;
-                    char control = cin.get();
-                    if(control == 'q') {
-                        break;
-                    }
-                    if(control == 'c') {
-                        count = 121;
-                    }
-                }
-            }
-        }
-    }
-    print_header();
-    analyze(buff1, buff2, tolerance);
-}
-
-void print_as_char(vector<float> buff){
-    char *buffer = (char*)buff.data();
-    int buffer_length = buff.size() * sizeof(float);
-    int line_len = 120;
-    int page_len = 20;
-    int count = 0;
-    int line_count = 0;
-    for(int i = 0; i < buffer_length; i++) {
-        if(buffer[i] >= 32 && buffer[i] <= 126) {
-            cout << buffer[i];
-            count++;
-            if(count%(line_len) == 0) {
-                cout << endl;
-                count = 0;
-                line_count++;
-            }
-        }
-        if(i%1000== 0) {
-            cout << endl << "i=" << i << "/" << buffer_length << "(" 
-                 << setprecision(4) << 100.0* ( i / (float)buffer_length) 
-                 << "%)" << endl;
-            count = 0;
-            line_count++;
-        }
-        if(line_count == page_len) {
-            line_count = 0;
-            char control = cin.get();
-            if('q' == control) {
-                break;
-            }
-            if('c' == control) {
-                line_count = page_len + 1;
-            }
-            if('g' == control) {
-                i = buffer_length - 20000;
-                count = 0;
-                line_count = 0;
-                cout << endl << endl 
-                     << "-------------------------------------------------------" 
-                     << endl << endl;
-            }
-        }
-    }
-    cout << endl;
-}
-
-
 int main( int argc, char **argv )
 {
     float tol_exp = -6;
@@ -177,18 +104,12 @@ int main( int argc, char **argv )
     vector<float> buff1 = get_data(argv[1]);
     vector<float> buff2 = get_data(argv[2]);
 
-    print_as_char(buff1);
-
     if(buff1.size() != buff2.size()) {
         cout << "error, files are different size: "  
              << buff1.size() << " for file1 vs "  
              << buff2.size() << " for file2" << endl;
         return 0;
     }
-    if(tol_range == -1){
-        print_diff(buff1, buff2, pow(10,tol_exp));
-                
-    } 
 
     print_header();
     for(int tol = tol_exp - tol_range; tol <= tol_exp + tol_range; tol++) {
